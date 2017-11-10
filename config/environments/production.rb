a0a22231e9eb0bfffb76b8cfb3cb2be8599b1850
Ovi Turcu <ovi_tsb@yahoo.com>
2017-11-10 00:49:47 -0500

    config.consider_all_requests_local       = true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Disable serving static files from the `/public` folder by default since
@@ -97,5 +97,5 @@ Rails.application.configure do
     url:':s3_domain_url',
     path: '/:class/:attachment/:id_partition/:style/:filename'
   }
-  
+
 end
