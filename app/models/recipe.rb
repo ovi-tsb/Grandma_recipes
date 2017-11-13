76bbf72fcbb70665ee480b0913e6aecb541d90f3
Ovi Turcu <ovi_tsb@yahoo.com>
2017-11-13 10:58:21 -0500

    recipe.rb with s3 settings

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,6 +11,14 @@ class Recipe < ApplicationRecord
   validates :title, :description, presence: true
   
   has_attached_file :image, styles: { medium: "400x400>" },
+    storage: :s3,
+    s3_host_name: 's3-us-east-2.amazonaws.com',
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    },
+    s3_region: ENV.fetch('AWS_REGION'),
     :url => "/assets/:basename.:extension",
     :path => ":rails_root/app/assets/images/:basename.:extension"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
