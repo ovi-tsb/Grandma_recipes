ae8602396c41e1898991413f109f21aa8450c90f
Ovi Turcu <ovi_tsb@yahoo.com>
2017-11-14 01:49:07 -0500

    recipe.rb no path

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,8 +11,8 @@ class Recipe < ApplicationRecord
   validates :title, :description, presence: true
   
   has_attached_file :image, styles: { medium: "400x400>" },
-    :url => "/assets/:basename.:extension",
-    :path => ":rails_root/app/app/assets/images/:basename.:extension"
+    :url => "/assets/:basename.:extension"#,
+    #:path => ":rails_root/app/assets/images/:basename.:extension"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
