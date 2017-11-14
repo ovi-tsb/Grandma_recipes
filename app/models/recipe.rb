e969545fd1cbc779cd8c03e86dbd6aeda53aea4c
Ovi Turcu <ovi_tsb@yahoo.com>
2017-11-14 02:04:12 -0500

    deleted /app/ from path

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -13,7 +13,7 @@ class Recipe < ApplicationRecord
   has_attached_file :image, styles: { medium: "400x400>" }, 
     default_url: "/images/:style/missing.png",
     :url => "/assets/:basename.:extension",
-    :path => ":rails_root/app/app/assets/images/:basename.:extension"
+    :path => ":rails_root/app/assets/images/:basename.:extension"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
