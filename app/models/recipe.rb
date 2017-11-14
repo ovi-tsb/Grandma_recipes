f2e5cf4a427d26c2b01a20e35ab880e03ff13f12
Ovi Turcu <ovi_tsb@yahoo.com>
2017-11-14 02:18:08 -0500

    new path in recipe.rb

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -11,7 +11,6 @@ class Recipe < ApplicationRecord
   validates :title, :description, presence: true
   
   has_attached_file :image, styles: { medium: "400x400>" }, 
-    default_url: "/images/:style/missing.png",
     :url => "/assets/:basename.:extension",
     :path => ":rails_root/app/assets/images/:basename.:extension"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
